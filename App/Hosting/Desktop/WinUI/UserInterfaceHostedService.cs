851a7b724558a6fc89529b966099c49e4e853e23
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2023-11-18 19:29:09 +0400

    refactor: upgrade to net8.0

--- App/Hosting/Desktop/WinUI/UserInterfaceHostedService.cs
+++ App/Hosting/Desktop/WinUI/UserInterfaceHostedService.cs
@@ -15,44 +15,32 @@ using Microsoft.Extensions.Logging.Abstractions;
 /// A long running service that will execute the User Interface thread.
 /// <para>
 /// Should be registered (only once) with the
-/// `AddHostedService&lt;UserInterfaceHostedService&gt;(IServiceCollection)` extension method.
+/// `AddHostedService&lt;UserInterfaceHostedService&gt;(IServiceCollection)`
+/// extension method.
 /// </para>
 /// </summary>
-public partial class UserInterfaceHostedService : IHostedService
+/// <remarks>
+/// <para>
+/// Expects the <see cref="UserInterfaceThread"/> and <see
+/// cref="HostingContext"/> singleton instances to be setup in the dependency
+/// injector.
+/// </para>
+/// </remarks>
+/// <param name="loggerFactory">We inject a <see cref="ILoggerFactory"/> to be
+/// able to silently use a <see cref="NullLogger"/> if we fail to obtain a <see
+/// cref="ILogger"/> from the Dependency Injector.</param>
+/// <param name="uiThread">The <see cref="UserInterfaceThread"/>
+/// instance.</param>
+/// <param name="context">The <see cref="HostingContext"/> instance.</param>
+public partial class UserInterfaceHostedService(
+    ILoggerFactory loggerFactory,
+    UserInterfaceThread uiThread,
+    HostingContext context)
+    : IHostedService
 {
-    private readonly ILogger<UserInterfaceHostedService> logger;
-    private readonly HostingContext context;
-    private readonly UserInterfaceThread uiThread;
-
-    /// <summary>
-    /// Initializes a new instance of the <see
-    /// cref="UserInterfaceHostedService"/> class will all parameters being
-    /// dependency injected.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// Expects the <see cref="UserInterfaceThread"/> and <see
-    /// cref="HostingContext"/> singleton instances to have been already setup
-    /// in the dependency injector.
-    /// </para>
-    /// </remarks>
-    /// <param name="loggerFactory">We inject a <see cref="ILoggerFactory"/> to
-    /// be able to silently use a <see cref="NullLogger"/> if we fail to obtain
-    /// a <see cref="ILogger"/> from the Dependency Injector.</param>
-    /// <param name="uiThread">The <see cref="UserInterfaceThread"/>
-    /// instance.</param>
-    /// <param name="context">The <see cref="HostingContext"/>
-    /// instance.</param>
-    public UserInterfaceHostedService(
-        ILoggerFactory loggerFactory,
-        UserInterfaceThread uiThread,
-        HostingContext context)
-    {
-        this.logger = loggerFactory?.CreateLogger<UserInterfaceHostedService>()
-            ?? NullLoggerFactory.Instance.CreateLogger<UserInterfaceHostedService>();
-        this.uiThread = uiThread;
-        this.context = context;
-    }
+    private readonly ILogger<UserInterfaceHostedService> logger = loggerFactory.CreateLogger<UserInterfaceHostedService>();
+    private readonly UserInterfaceThread uiThread = uiThread;
+    private readonly HostingContext context = context;
 
     /// <inheritdoc />
     public Task StartAsync(CancellationToken cancellationToken)
